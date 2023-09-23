3020273683b5c0495e72458bcffef2ed5998a39a
Suparan <suparansharmap@gmail.com>
2023-09-23 22:28:51 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import CreateTask from './components/CreateTask'
 import ListTasks from './components/ListTasks'
 import toast, { Toaster } from 'react-hot-toast';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 function App() {
   const[tasks,setTasks]= useState([]);
@@ -11,12 +13,14 @@ setTasks(JSON.parse(localStorage.getItem("tasks")));
 console.log(tasks);
   return (
     <>
+    <DndProvider backend={HTML5Backend}>
     <Toaster />
-      <div className="bg-slate-100 w-screen h-screen flex flex-col items-center pt-32 gap-16">
+      <div className="bg-slate-100 w-screen h-screen flex flex-col items-center p-3 pt-32 gap-16">
 
       <CreateTask tasks={tasks} setTasks={setTasks} />
       <ListTasks tasks={tasks} setTasks={setTasks}/>
       </div>
+      </DndProvider>
     </>
   )
 }
--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
 import toast from 'react-hot-toast';
 
 const ListTasks = ({ tasks, setTasks }) => {
@@ -34,34 +35,57 @@ export default ListTasks
 
 
 const Section = ({ status, tasks, setTasks, todos, inprogress, done }) => {
-    console.log("todos",inprogress);
+
+
+        const [{ isOver }, drop] = useDrop(() => ({
+        accept: "task",
+        drop:(item) => addItemToSection(item.id),
+        collect: (monitor) => ({
+            isOver: !!monitor.isOver()
+        })
+    }))
+
     let text = "Todo";
     let bg = "bg-slate-500";
     let tasksToMap = todos
 
-    if(status === "inprogress"){
+    if (status === "inprogress") {
         text = "In Progress";
         bg = "bg-purple-500";
         tasksToMap = inprogress;
     }
 
 
-    if(status === "done"){
-        text = "In Progress";
+    if (status === "done") {
+        text = "Done";
         bg = "bg-green-500";
         tasksToMap = done;
     }
 
+    const addItemToSection = (id) =>{
+       setTasks((prev) =>{
+       const mTasks = prev.map(t =>{
+        if(t.id === id){
+            return { ...t,status:status}
+        }
+        return t;
+       })
+       localStorage.setItem("tasks",JSON.stringify(mTasks))
+       toast.success("Changes")
+        return mTasks;
+       })
+    }
+
     return (
-        <div className={`w-64`}>
+        <div ref={drop} className={`w-64 rounded-md p-2  ${isOver ? "bg-slate-200" : ""}`}>
 
 
             {/* <h2>{status}</h2> */}
             <Header text={text} bg={bg} count={tasksToMap.length} />
             {
-                tasksToMap.length > 0 && 
-                tasksToMap.map((task)=>(
-                    <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks}/>
+                tasksToMap.length > 0 &&
+                tasksToMap.map((task) => (
+                    <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks} />
                 ))
             }
         </div>
@@ -86,24 +110,38 @@ const Header = ({ text, bg, count }) => {
 
 
 
-const Task = ({task, tasks, setTasks }) => {
-    const handleRemove =  (id) =>{
-console.log(id);
+const Task = ({ task, tasks, setTasks }) => {
 
-const fTasks = tasks.filter(t => t.id !== id);
 
-setTasks(fTasks);
-toast.success("Successfully Removed")
+    const [{ isDragging }, drag] = useDrag(() => ({
+        type: "task",
+        item:{id:task.id},
+        collect: (monitor) => ({
+            isDragging: !!monitor.isDragging()
+        })
+    }))
+
+    console.log(isDragging);
+
+    const handleRemove = (id) => {
+
+
+        const fTasks = tasks.filter(t => t.id !== id);
+        localStorage.setItem("tasks", JSON.stringify(fTasks))
+        setTasks(fTasks);
+        toast.success("Successfully Removed")
     }
     return (
-        <div className={`relative p-4 mt-8 shadow-md rounded-md cursor-grab`}>
+        <div
+            ref={drag}
+            className={`relative p-4 mt-8 shadow-md rounded-md cursor-grab ${isDragging?"opacity-25":"opacity-100"}`}>
             <p>{task.name}</p>
-            <button className="absolute bottom-1 right-1 text-slate-400" 
-            onClick={()=>handleRemove(task.id)}
+            <button className="absolute bottom-1 right-1 text-slate-400"
+                onClick={() => handleRemove(task.id)}
             >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
 
             </button>
         </div>
